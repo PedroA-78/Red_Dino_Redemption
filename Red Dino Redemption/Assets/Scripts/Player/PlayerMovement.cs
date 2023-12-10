68927b057bba36d9819d214eb4f206f10420c2bb
Pedro A. Ramos <67925461+PedroA-78@users.noreply.github.com>
2023-12-10 14:05:30 -0300

    Player Movement

---
+++ Red Dino Redemption/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform camTransform;
+    Vector2 rotationMouse;
+    public int sensiblity;
+    public int velocity;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        float rotationX = mouseX * sensiblity * Time.deltaTime;
+        float rotationY = mouseY * sensiblity * Time.deltaTime;
+
+        rotationMouse = new Vector2(rotationX, rotationY);
+
+        Vector3 dir = new Vector3(x, 0, y) * velocity;
+
+        transform.Translate(dir * Time.deltaTime);
+
+        transform.Rotate(new Vector3(
+            0,
+            rotationMouse.x,
+            0
+        ));
+
+        rotationMouse.y = Mathf.Clamp(rotationMouse.y, -20, 20);
+
+        camTransform.transform.Rotate(new Vector3(
+            -rotationMouse.y,
+            0,
+            0
+        ));
+    }
+}

0cab7fb107f17ee95f6daf3411faf274a9864341
Pedro A. Ramos <67925461+PedroA-78@users.noreply.github.com>
2023-12-17 14:20:05 -0300

    Animations added
    - Adicionado os modelos 3d da Pistola e do Machado;
    - Adicionado animações de correr;
    - Adicionado animações de andar do player com o machado na mão;
    - Adicionado animação do player atacando com o machado;
    - Adicionado animações de andar e correr do player com a pistola na mão;
    - Adicionado um sistema p/ escolher a arma pelo scroll do mouse;
    - Adicionado uma UI p/ ter retorno de qual arma está ativa (somente testes);

--- Red Dino Redemption/Assets/Scripts/Player/PlayerMovement.cs
+++ Red Dino Redemption/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,6 +14,10 @@ public class PlayerMovement : MonoBehaviour
     public int velocity;
 
     private Animator anim;
+    public GameObject Axe;
+    public GameObject Pistol;
+    public GameObject WeaponUI;
+    private GameObject crrWeapon;
     
     void Start()
     {
@@ -17,26 +25,110 @@ public class PlayerMovement : MonoBehaviour
         Cursor.visible = false;
         
         anim = GetComponent<Animator>();
+
+        Axe.SetActive(false);
+        Pistol.SetActive(false);
+
+        crrWeapon = Pistol;
+        WeaponUI.SetActive(false);
+        
     }
 
-    void Update()
+    async void Update()
     {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis ("Horizontal");
+        float y = Input.GetAxis ("Vertical");
+        bool click = Input.GetMouseButtonDown(0);
 
-        anim.SetFloat("horizontalMove", x);
-        anim.SetFloat("verticalMove", y);
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0f) {
+            crrWeapon = Pistol;
+            await swipeIcon("Pistol");
+            
+        } else if (scroll < 0f) {
+            crrWeapon = Axe;
+            await swipeIcon("Axe");
+        }
+
+
+        if (Input.GetMouseButtonDown(1)){
+            if (crrWeapon.name == "Pistol") {
+                anim.SetBool("mousePressed", true);
+            } else if (crrWeapon.name == "Axe 1") {
+                anim.SetBool("scrollPressed", true);
+            }
+            anim.SetBool("triggered", false);
+            anim.SetFloat("verticalMove", y);
+            anim.SetFloat("horizontalMove", x);
+
+            crrWeapon.SetActive(true);
+
+        } else if (Input.GetMouseButtonDown(2)) {
+            Axe.SetActive(true);
+
+            anim.SetBool("scrollPressed", true);
+            anim.SetFloat("verticalMove", y);
+            anim.SetFloat("horizontalMove", x);
+
+            if (Input.GetMouseButtonDown(0)) {
+                anim.SetBool("triggered", true);
+            }
+
+                    
+
+        } else if (Input.GetKeyDown(KeyCode.LeftShift) && (x != 0 || y != 0)) {
+            anim.SetBool("shiftPressed", true);
+            anim.SetFloat("verticalMove", y);
+            anim.SetFloat("horizontalMove", x);
+            Debug.Log("Horizontal: " + x);
+            Debug.Log("Vertical: " + y);
+
+        } else {
+            anim.SetFloat("verticalMove", y);
+            anim.SetFloat("horizontalMove", x);
+        }
+
+        if (Input.GetMouseButtonUp(1)) {
+            if (crrWeapon.name == "Pistol") {
+                anim.SetBool("mousePressed", false);
+            } else if (crrWeapon.name == "Axe 1") {
+                anim.SetBool("scrollPressed", false);
+            }
+            crrWeapon.SetActive(false);
+            // await Task.Delay(450);
+
+            // anim.SetBool("triggered", true);
+            // Debug.Log("Tested");
+        } else if (Input.GetMouseButtonUp(2)) {
+            anim.SetBool("scrollPressed", false);
+            Axe.SetActive(false);
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            anim.SetBool("shiftPressed", false);
+        } 
+
+        // if (Input.GetMouseButtonUp(0)) {
+        //         anim.SetBool("triggered", false);
+        // }
+
+        if (anim.GetBool("scrollPressed") && click) {
+            anim.SetBool("triggered", true);
+
+            await Task.Delay(1000);
+
+            anim.SetBool("triggered", false);
+        }
+
 
         float rotationX = mouseX * sensiblity * Time.deltaTime;
         float rotationY = mouseY * sensiblity * Time.deltaTime;
 
         rotationMouse = new Vector2(rotationX, rotationY);
 
-        // Vector3 dir = new Vector3(x, 0, y) * velocity;
+        Vector3 dir = new Vector3(x, 0, y) * velocity;
 
-        // transform.Translate(dir * Time.deltaTime);
+        transform.Translate(dir * Time.deltaTime);
 
         transform.Rotate(new Vector3(
             0,
@@ -44,12 +136,30 @@ public class PlayerMovement : MonoBehaviour
             0
         ));
 
-        rotationMouse.y = Mathf.Clamp(rotationMouse.y, -20, 20);
+        // rotationMouse.y = Mathf.Clamp(rotationMouse.y, -20, 20);
 
-        camTransform.transform.Rotate(new Vector3(
-            -rotationMouse.y,
-            0,
-            0
-        ));
+        // camTransform.transform.Rotate(new Vector3(
+        //     -rotationMouse.y,
+        //     0,
+        //     0
+        // ));
+
+        
+    }
+
+    public async Task swipeIcon(String Item){
+        if (Item == "Pistol") {
+            WeaponUI.transform.GetChild(1).transform.gameObject.SetActive(true);
+            WeaponUI.transform.GetChild(2).transform.gameObject.SetActive(false);
+        } else if (Item == "Axe") {
+            WeaponUI.transform.GetChild(1).transform.gameObject.SetActive(false);
+            WeaponUI.transform.GetChild(2).transform.gameObject.SetActive(true);
+        }
+
+        WeaponUI.SetActive(true);
+
+        await Task.Delay(3000);
+
+        WeaponUI.SetActive(false);
     }
 }
